e31e279e51a21f355a1ada7d28283bce917f4a70
ReTims <renatatims@gmail.com>
2023-01-30 16:59:28 -0600

    Created models folder + User.js basic model structure

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+ 
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
