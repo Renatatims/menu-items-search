32be3a551f7355e5b7148512a368552ec1636517
ReTims <renatatims@gmail.com>
2023-01-31 16:17:09 -0600

    Included icon and search bar to navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -8,6 +8,15 @@ function Navbar() {
       <div className="mx-auto justify-between px-4 md:flex md:items-center md:px-8 lg:max-w-7xl">
         <div>
           <div className="flex items-center justify-between py-3 md:block md:py-5 lg:block lg:py-5">
+            <div>
+              <a href="/">
+                <img
+                  src={require("./foodSearchIcon.ico")}
+                  className="w-14"
+                  alt="icon"
+                ></img>
+              </a>
+            </div>
             <div className="md:hidden">
               <button
                 className="rounded-md p-2 text-gray-700 outline-none focus:border focus:border-gray-400"
@@ -53,7 +62,30 @@ function Navbar() {
             }`}
           >
             <ul className="items-center justify-center space-y-8 text-xl md:flex md:space-x-6 md:space-y-0">
-              <li className="text-white hover:text-indigo-200">Search</li>
+              <div class="relative md:block">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg
+                    class="w-5 h-5 text-gray-500"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <span class="sr-only">Search icon</span>
+                </div>
+                <input
+                  type="text"
+                  id="search-navbar"
+                  className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Search..."
+                ></input>
+              </div>
               <li className="text-white hover:text-indigo-200">Favorites</li>
               <div className="mt-3 space-y-2 md:inline-block">
                 <button className="inline-block w-full rounded-md bg-gray-600 px-4 py-2 text-center text-white shadow hover:bg-gray-800">
